a48ac130e63f42281ad4388f4f85cfd4e039b403
Kevin Tuffner <kevin.tuffner@thinkahead.com>
2019-05-09 14:47:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,105 +5,113 @@ provider "azurerm" {
 
 # Create Resource Group in Azure
 
-resource "azurerm_resource_group" "tuffyrg01" {
-    name     = "demo01"
-    location = "eastus"
+resource "azurerm_resource_group" "tuffy01" {
+    name     = "tuffy01"
+    location = "westus"
 
     tags {
         environment = "Terraform Demo"
     }
 }
 
-# Create Azure Virtual Network for demo01
+# create a virtual network
+resource "azurerm_virtual_network" "tuffyvnet" {
+    name = "acctvn"
+    address_space = ["10.0.0.0/16"]
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
+}
 
-resource "azurerm_virtual_network" "tuffyvn01" {
-    name                = "demo-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.tuffyrg01.name}"
+# create subnet
+resource "azurerm_subnet" "tuffysubnet" {
+    name = "acctsub"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
+    virtual_network_name = "${azurerm_virtual_network.tuffyvnet.name}"
+    address_prefix = "10.0.2.0/24"
+}
+
+# create public IP
+resource "azurerm_public_ip" "tuffypubip" {
+    name = "tuffytestip"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
+    public_ip_address_allocation = "dynamic"
 
     tags {
-        environment = "Terraform Demo"
+        environment = "TerraformDemo"
     }
 }
 
-# Create Subnet within Azure Virtual Network for demo
-
-resource "azurerm_subnet" "tuffysubnet01" {
-    name                 = "Public-Subnet"
-    resource_group_name  = "${azurerm_resource_group.tuffyrg01.name}"
-    virtual_network_name = "${azurerm_virtual_network.tuffyvn01.name}"
-    address_prefix       = "10.0.2.0/24"
-}
-
-# Create Public IP Address For Azure VM Instance
-
-resource "azurerm_public_ip" "tuffytfpubip01" {
-    name                         = "demopublicip01"
-    location                     = "eastus"
-    resource_group_name          = "${azurerm_resource_group.tuffyrg01.name}"
-    allocation_method            = "Dynamic"
-
-    tags {
-        environment = "Terraform Demo"
-    }
-}
-
-# Create Network Security Group & Rule for Demo Instance
-
-resource "azurerm_network_security_group" "tuffynsg01" {
-    name                = "TuffyNetworkSecurityGroup01"
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.tuffyrg01.name}"
-
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags {
-        environment = "Terraform Demo"
-    }
-}
-
-# Create Azure Network Interface Card for Demo Instance
-
-# Create Network Interface Card for Azure VM
-
-resource "azurerm_network_interface" "tuffynic01" {
-    name                = "DemoNIC01"
-    location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.tuffyrg01.name}"
-    network_security_group_id = "${azurerm_network_security_group.tuffynsg01.id}"
+# create network interface
+resource "azurerm_network_interface" "tuffynic" {
+    name = "tuffytfni"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
 
     ip_configuration {
-        name                          = "Demo01NicConfiguration"
-        subnet_id                     = "${azurerm_subnet.tuffysubnet01.id}"
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.tuffytfpubip01.id}"
-    }
-
-    tags {
-        environment = "Terraform Demo"
+        name = "testconfiguration1"
+        subnet_id = "${azurerm_subnet.tuffysubnet.id}"
+        private_ip_address_allocation = "static"
+        private_ip_address = "10.0.2.5"
+        public_ip_address_id = "${azurerm_public_ip.tuffypubip.id}"
     }
 }
 
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "tuffystorageaccount" {
-    name                        = "diagdemo01"
-    resource_group_name         = "${azurerm_resource_group.tuffyrg01.name}"
-    location                    = "eastus"
-    account_tier                = "Standard"
-    account_replication_type    = "LRS"
+# create storage account
+resource "azurerm_storage_account" "tuffystorageacct01" {
+  name                     = "tuffystorageacct01"
+  resource_group_name      = "${azurerm_resource_group.tuffy01.name}"
+  location                 = "westus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+# create storage container
+resource "azurerm_storage_container" "tuffystoragecontainer" {
+    name = "vhd"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
+    storage_account_name = "${azurerm_storage_account.tuffystorageacct01.name}"
+    container_access_type = "private"
+    depends_on = ["azurerm_storage_account.tuffystorageacct01"]
+}
+
+# create virtual machine
+resource "azurerm_virtual_machine" "tuffytfvm01" {
+    name = "terraformvm"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.tuffy01.name}"
+    network_interface_ids = ["${azurerm_network_interface.tuffynic.id}"]
+    vm_size = "Standard_A0"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "14.04.2-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "myosdisk"
+        vhd_uri = "${azurerm_storage_account.tuffystorageacct01.primary_blob_endpoint}${azurerm_storage_container.tuffystoragecontainer.name}/myosdisk.vhd"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name = "hostname"
+        admin_username = "testadmin"
+        admin_password = "Password1234!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
 
     tags {
-        environment = "Terraform Demo"
+        environment = "staging"
     }
 }
